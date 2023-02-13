15b9f0c2316ffbaf22c0b0cbcab8ebc3df571915
andream-castronovo <andreamaria.castronovo.stud@ispascalcomandini.it>
2023-02-13 13:40:49 +0100

    Aggiungere i file di progetto.

---
+++ SharedProject_Azienda/Persona.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedProject_Azienda
+{
+    abstract class Persona
+    {
+        string _nome;
+        string _cognome;
+
+        public Guid ID { get; set; }
+        
+        public string Nome
+        {
+            get => _nome;
+            set
+            {
+                // TODO: Fare controlli
+                _nome = value;
+            }
+        }
+
+        public string Cognome
+        {
+            get => _cognome;
+            set
+            {
+                // TODO: Fare controlli
+                _cognome = value;
+            }
+        }
+
+        public Persona()
+        {
+            _nome = "<no_name>"
+            _cognome = "<no_surname>"
+        }
+    }
+}
